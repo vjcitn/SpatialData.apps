6f6f9c0edd55fa2c359086b173e693729e70bf06
vjcitn <stvjc@channing.harvard.edu>
2024-12-14 11:29:03 -0500

    progress but not closing path visually

--- inst/sketches/app2.R
+++ inst/sketches/app2.R
@@ -8,9 +8,29 @@ library(ggplot2)
 library(sf)
 library(DT)
 if (!exists("brdat")) brdat = Breast2fov_10x()
+ brsh = shape(brdat)
+ # some points to form a subregion
+ pick = structure(list(x = c(355.185319678255, 353.624236083554, 510.140679646861, 
+    496.285253053229, 355.185319678255), y = c(-471.421489018199, 
+    -591.83220661942, -601.010859954238, -467.21109758021, -471.421489018199
+    )), row.names = c(NA, -5L), class = "data.frame")
+ pick_poly = st_polygon(list(abs(data.matrix(pick))))
+ brshpoly = st_as_sf(brsh@data)
+ trim = st_intersects(brshpoly, pick_poly, sparse=FALSE)
+ sh251 = brshpoly[i=which(trim[,1]),]
+ picked = ShapeFrame(as.data.frame(sh251))
+ #    nd = data.frame(x=xs, y=ys)
+ #    p1 = p1+geom_path(data=nd, aes(x=x,y=y), colour="red", linewidth=3)
+ #    }
 
-xs = NULL
-ys = NULL
+ if (!("pick" %in% shapeNames(brdat))) shapes(brdat)$pick = picked
+
+
+docrop = function(spdat=brdat, cropview="pick") {
+xs <<- NULL
+ys <<- NULL
+baseplot = plotSpatialData() + plotShape(spdat, c="black") + geom_path(data=pick, aes(x=x,y=abs(y)), colour="red", linewidth=2)  # FIXME pick not general
+brshpoly = st_as_sf(shape(spdat)@data)
 ui = fluidPage(
  sidebarLayout(
   sidebarPanel(
@@ -18,6 +38,7 @@ ui = fluidPage(
    helpText("select center of circle"),
    DT::dataTableOutput("pathdat"),
    actionButton("usepath", "use"), 
+   actionButton("closepath", "closepath"), 
    actionButton("clearpath", "clearpath"), 
    actionButton("stopapp", "stop app"), 
    width=2
@@ -30,6 +51,9 @@ ui = fluidPage(
     tabPanel("cropped",
      plotOutput("crop", width="800px", height="800px"),
      verbatimTextOutput("ans")
+     ),
+    tabPanel("crop2",
+     plotOutput("crop2", width="800px", height="800px"),
      )
     )
    )
@@ -37,36 +61,33 @@ ui = fluidPage(
  )
 
 server = function(input, output, session) {
- brsh = shape(brdat)
- # some points to form a subregion
- pick = structure(list(x = c(355.185319678255, 353.624236083554, 510.140679646861, 
-    496.285253053229, 355.185319678255), y = c(-471.421489018199, 
-    -591.83220661942, -601.010859954238, -467.21109758021, -471.421489018199
-    )), row.names = c(NA, -5L), class = "data.frame")
- pick_poly = st_polygon(list(abs(data.matrix(pick))))
- brshpoly = st_as_sf(brsh@data)
- trim = st_intersects(brshpoly, pick_poly, sparse=FALSE)
- sh251 = brshpoly[i=which(trim[,1]),]
- picked = ShapeFrame(as.data.frame(sh251))
- if (!("pick" %in% shapeNames(brdat))) shapes(brdat)$pick = picked
-
  output$crop = renderPlot({
-   plotSpatialData() + plotShape(brdat, "pick", c="black")
+   plotSpatialData() + plotShape(spdat, cropview, c="black")
+   })
+ output$crop2 = renderPlot({
+   shapes(spdat)$crop2 = closeddf()
+   plotSpatialData() + plotShape(spdat, "crop2", c="black")
    })
 
  output$cells = renderPlot({
    input$plot_click
    input$clearpath
-   p1 = plotSpatialData() + plotShape(brdat, c="black") + geom_path(data=pick, aes(x=x,y=abs(y)), colour="red",
-      linewidth=3)  # full dataset
+   input$closepath
+#   p1 = plotSpatialData() + plotShape(spdat, c="black") + geom_path(data=pick, aes(x=x,y=abs(y)), colour="red",  # FIXME pick not general
+#      linewidth=3)  # full dataset
+   extra = NULL
    if (length(xs)>1) {
      nd = data.frame(x=xs, y=ys)
-     p1 = p1+geom_path(data=nd, aes(x=x,y=y), colour="red", linewidth=3)
+print(xs)
+print(nd)
+     extra = geom_path(data=nd, aes(x=x,y=y), colour="red", linewidth=3)
      }
-   p1
+   if (!is.null(extra)) baseplot + extra
+   else baseplot
    })
  output$pathdat <- DT::renderDataTable({
            input$clearpath
+           input$closepath
            xs <<- c(xs, input$plot_click$x)
            ys <<- c(ys, input$plot_click$y)
            #cat(xs)
@@ -77,7 +98,19 @@ server = function(input, output, session) {
     })
  pathdf <- eventReactive(input$usepath, {
     input$clearpath
+    input$closepath
     data.frame(x=xs, y=ys)
+    })
+ closeddf <- eventReactive(input$closepath, {
+    input$clearpath
+    ans = data.frame(x=xs, y=ys)
+    ans = rbind(ans, ans[1,])
+    xs <<- ans[,1]  # update vecs used for rendering on base plot
+    ys <<- ans[,2]
+    pick_poly = st_polygon(list(abs(data.matrix(ans))))
+    trim = st_intersects(brshpoly, pick_poly, sparse=FALSE)
+    shtrim = brshpoly[i=which(trim[,1]),]
+    ShapeFrame(as.data.frame(shtrim))
     })
  observeEvent(input$clearpath, {
     xs <<- NULL
@@ -87,4 +120,5 @@ server = function(input, output, session) {
 }
 
 runApp(list(ui=ui, server=server))
+}
 
